d4c46cd79a2e2d43defe9200b778b14d94a1815a
Mark Needham <mark.needham@neotechnology.com>
2014-09-21 19:54:35 +0100

    initial

---
+++ src/clojure_neo4j_hackathon/routes/home.clj
@@ -0,0 +1,33 @@
+(ns clojure-neo4j-hackathon.routes.home
+  (:require [compojure.core :refer :all]
+            [clojurewerkz.neocons.rest        :as nr]
+            [clojurewerkz.neocons.rest.cypher :as cy]
+            [clojure-neo4j-hackathon.layout :as layout]
+            [clojure-neo4j-hackathon.util :as util]
+            [clojure.walk :as walk]))
+
+(def conn (nr/connect "http://localhost:7530/db/data/"))
+
+(def all-movies-query "MATCH (m:Movie)
+                       RETURN m
+                       LIMIT {limit}")
+
+(defn get-movies
+  [limit]
+  (let [result (->> (cy/tquery conn all-movies-query {:limit limit})
+                    walk/keywordize-keys)]
+    result))
+
+(defn home-page []
+  (let [result (get-movies 50)]
+    (println result)
+    (layout/render
+     "home.html"
+     {:result (map  #(->> % :m :data) result) })))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
---
+++ test/clojure_neo4j_hackathon/test/handler.clj
@@ -0,0 +1,13 @@
+(ns clojure-neo4j-hackathon.test.handler
+  (:use clojure.test
+        ring.mock.request
+        clojure-neo4j-hackathon.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))))
+
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
